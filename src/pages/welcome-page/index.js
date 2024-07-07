95826f04d9cdf800f09ee4b4724538d50444934c
Kwabena Asumadu <codinnovations@gmail.com>
2024-07-07 04:03:46 -0700

    modified

--- src/pages/welcome-page/index.js
+++ src/pages/welcome-page/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "../../styles/welcome.module.css";
+import Image from "next/image";
+import Logo from "../../../public/logo-w-2.png";
 
 function Index() {
   return (
     <>
       <div className={styles.welcomeContainer}>
-        <div className={styles.backgroundImage}></div>
+        <div className={styles.backgroundImage}>
+          <Image
+            src={Logo}
+            alt="company-logo"
+            
+            className={styles.image}
+          />
+        </div>
       </div>
     </>
   );
