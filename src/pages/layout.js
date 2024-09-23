9fbc26701e51da02259bce4f83630135fccfbbd9
Kwabena Asumadu <codinnovations@gmail.com>
2024-09-23 22:19:11 +0100

    remove first header

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,5 +1,4 @@
 import React from "react";
-import FirstHeader from "../pages/first-header";
 import Navigation from '../pages/navigation-bar'
 import Showcase from "../pages/showcase";
 import Footer from '../pages/footer'
@@ -7,7 +6,6 @@ import Footer from '../pages/footer'
 function Layout({ children }) {
   return (
     <div>
-      <FirstHeader />
       <Navigation />
       <Showcase />
       {children}
