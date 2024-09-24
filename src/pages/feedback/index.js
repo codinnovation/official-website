b219499dda97edc61fc55644570211cc9511fbb2
Kwabena Asumadu <codinnovations@gmail.com>
2024-09-24 11:38:11 +0100

    added footer an feedback

--- src/pages/feedback/index.js
+++ src/pages/feedback/index.js
@@ -1,70 +1,68 @@
 import React, { useEffect, useState } from "react";
 import styles from '../../styles/feedback.module.css'
+import CustomerImage from '../../../public/img1 (3).png'
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { db } from "../../../firebase.config";
 import { get, ref } from "firebase/database";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
 
 function Index() {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dbRef = ref(db, "feedback");
-        const response = await get(dbRef);
-        const data = response.val();
-
-        if (data && typeof data === "object") {
-          const dataArray = Object.entries(data).map(([key, value]) => ({
-            key,
-            ...value,
-          }));
-          setData(dataArray);
-        } else {
-          setData([]);
-        }
-      } catch (error) {
-        console.error("Error fetching data:");
-        setData([]);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.container_header}>
-          <h1>What our clients say</h1>
+      <div className={styles.feedbackContainer}>
+        <div className={styles.feedbackContainerHeader}>
+          <h1>Hear from our</h1>
+          <h1>clients</h1>
         </div>
 
-        {data.length === 0 ? (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-              padding: "10px",
-              color:"#008374",
-            }}
-          >
-            <CircularProgress />
-            <em>Oops!!!</em>
-          </Box>
-        ) : (
-          data.map((item, index) => (
-            <div className={styles.container_items} key={index}>
-              <div className={styles.container_item}></div>
-              <div className={styles.container_item}></div>
+        <div className={styles.feedbackContent}>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ infinity: true }}
+            className={styles.feedback}>
+            <div className={styles.feedbackImage}>
+              <Image src={CustomerImage} width={900} height={900} alt="customer-image" />
             </div>
-          ))
-        )}
+            <h1>Kwabena Sakyi Asumadu</h1>
+            <p>Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+          </motion.div>
 
-        <div className={styles.container_button}>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            viewport={{ infinity: true }}
+            className={styles.feedback}>
+            <div className={styles.feedbackImage}>
+              <Image src={CustomerImage} width={900} height={900} alt="customer-image" />
+            </div>
+            <h1>Kwabena Sakyi Asumadu</h1>
+            <p>Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            viewport={{ infinity: true }}
+            className={styles.feedback}>
+            <div className={styles.feedbackImage}>
+              <Image src={CustomerImage} width={900} height={900} alt="customer-image" />
+            </div>
+            <h1>Kwabena Sakyi Asumadu</h1>
+            <p>Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+          </motion.div>
+        </div>
+
+        <div className={styles.feedbackButtons}>
           <ArrowBackIcon className={styles.icon} />
           <ArrowForwardIcon className={styles.icon} />
         </div>
--- src/pages/footer/index.js
+++ src/pages/footer/index.js
@@ -1,24 +1,58 @@
 import React from "react";
-import styles from '../../styles/footer.module.css'
-import FmdGoodIcon from "@mui/icons-material/FmdGood";
-import EmailIcon from "@mui/icons-material/Email";
-import PhoneIcon from "@mui/icons-material/Phone";
-import FacebookIcon from "@mui/icons-material/FacebookOutlined";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import LinkedinIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import Link from "next/link";
+import styles from '../../styles/footer.module.css';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import XIcon from '@mui/icons-material/X';
+import { motion } from "framer-motion";
+import Image from "next/image";
+import CODLOGO from '../../../public/logo-w-2.png'
 
-function Index() {
+function Footer() {
   return (
     <>
-      <div className={styles.footerContainer}>
+      <motion.div
+        className={styles.footerContainer}
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: true, amount: 0.1 }}
+      >
         <div className={styles.footerContent}>
-          <div className={styles.logoContainer}></div>
+          <motion.div
+            className={styles.companyName}
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.8, ease: "easeInOut" }}
+            viewport={{ once: true, amount: 0.1 }}
+          >
+            <Image src={CODLOGO} width={900} height={900} alt="cod-logo" />
+          </motion.div>
+
+          <motion.div
+            className={styles.copyright}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 0.8, ease: "easeInOut" }}
+            viewport={{ once: true, amount: 0.1 }}
+          >
+            <p>© {new Date().getFullYear()} COD Innovations. All Rights Reserved</p>
+          </motion.div>
+
+          <motion.div
+            className={styles.socialMedia}
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 1.5, duration: 0.8, ease: "easeInOut" }}
+            viewport={{ once: true, amount: 0.1 }}
+          >
+            <FacebookIcon className={styles.icon} />
+            <WhatsAppIcon className={styles.icon} />
+            <XIcon className={styles.icon} />
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
 
-export default Index;
+export default Footer;
