a3329822b04b9fd4f09aa6168620d58f0d440dd7
codinnovation <codinnovations@gmail.com>
2025-02-09 12:10:30 +0000

    update index footer

--- src/pages/home-page/footer/index.tsx
+++ src/pages/home-page/footer/index.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <>
       <div className="w-full bg-[#f85a40]  mt-[15%] xl:mt-[13%] lg:mt-[10%] xl:h-[20vh] lg:h-[25vh] h-[20vh] ">
         <div className="lg:w-10/12 xl:w-8/12 mx-auto  h-[100%] w-11/12 gap-[20px] flex justify-center items-center flex-col">
-          {/**Icons */}
+        
           <div className="flex justify-center items-center space-x-6">
             <i className="ri-facebook-line text-white flex justify-center items-center bg-[#524568] xl:text-[1.5em] lg:text-[1.3em] xl:w-[50px] xl:h-[50px] lg:w-[40px] lg:h-[40px] rounded-[50%] w-[40px] h-[40px] text-[1.2em]"></i>
 
